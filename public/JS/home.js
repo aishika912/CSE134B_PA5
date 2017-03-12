51b9c2b24fe5ed3bb7f28ebb14b8818671600685
Anindya Basu <abasu@ucsd.edu>
2017-03-11 23:45:51 -0800

    update

--- public/JS/home.js
+++ public/JS/home.js
@@ -38,28 +38,40 @@ function displaySticksFn(data) {
 	//console.log("MADE IT");
 	var lipsticks = data.val();
 	var keys = Object.keys(lipsticks);
+
+	var own_total = 0;
+	var want_total = 0;
+
+	var own_number = 0;
+	var want_number = 0;
 	//console.log("MADE IT");
 	for (var i = 0; i < keys.length; ++i) {
 		var k = keys[i];
 		if (lipsticks[k].own == true) {
 			addStickToOwn(k);
+			/* color chart */
 			var colorBox = document.createElement("div");
 			colorBox.className += "color-box"
 			colorBox.style.backgroundColor = lipsticks[k].color;
 			document.getElementById("data-owned").appendChild(colorBox);
+
+			/* price analysis */
+			own_number++;
+			own_total += parseInt(lipsticks[k].price);
 		}
 		else {
+			/* color chart */
 			addStickToWant(k);
 			var colorBox = document.createElement("div");
 			colorBox.className += "color-box"
 			colorBox.style.backgroundColor = lipsticks[k].color;
 			document.getElementById("data-wanted").appendChild(colorBox);
+
+			/* price analysis */
+			want_number++;
+			want_total += parseInt(lipsticks[k].price);
 		}
 		//console.log(k);
-	}
-
-	//log(data.val());
-
 }
 function displayWantData() {
 	firebase.auth().onAuthStateChanged(function(currentUser) {
